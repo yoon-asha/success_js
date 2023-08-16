75622f560d06239a8a0c0b24e68e7fccfc4955d1
yoon-asha <yoon.asha97@gmail.com>
2023-08-16 13:55:21 +0900

    db fixed

--- back/database.js
+++ back/database.js
@@ -1,4 +1,10 @@
 const mysql = require('mysql2/promise')
-const dbSecret = require('./secret')
+const { dbSecret } = require('./secret')
 
-exports.pool = mysql.createPool(dbSecret)
+exports.pool = mysql.createPool({
+  host: dbSecret.host,
+  user: dbSecret.user,
+  port: dbSecret.port,
+  password: dbSecret.password,
+  database: dbSecret.database,
+})
